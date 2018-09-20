b234a8bfbdee1d989946ddf77de481d131a6fb39
Joe <joao.paulo.miquilini@gmail.com>
2018-09-20 09:25:37 -0300

    save and merge EmitenteDAO

--- src/model/dao/EmitenteDAO.java
+++ src/model/dao/EmitenteDAO.java
@@ -7,11 +7,32 @@ package model.dao;
 
 import connection.ConnectionFactory;
 import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import model.bean.Emitente;
 
 /**
  *
  * @author Joe
  */
 public class EmitenteDAO {
-
+    
+    EntityManager em = new ConnectionFactory().getConnection();
+    
+    public Emitente save(Emitente emitente){
+        try {
+            em.getTransaction().begin();
+            if(emitente.getId() == null){
+                em.persist(emitente);
+            }
+            else{
+                em.merge(emitente);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+        return emitente;
+    }
 }
