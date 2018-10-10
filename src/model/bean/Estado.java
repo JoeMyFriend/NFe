3a012f290a4c60cf1bbe5bf47b2efe81d19f7464
Joe <joao.paulo.miquilini@gmail.com>
2018-10-10 17:07:36 -0300

    Bean e DAO, Cidade e Estado

---
+++ src/model/bean/Estado.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.bean;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+@Table(name = "tblendereco_estado")
+public class Estado implements Serializable {
+    @Id
+    private Integer id;
+
+    @Column(name = "estado")
+    private String nome;
+    private String sigla;
+
+    @Column(name = "codigo_ibge")
+    private Integer codigoIbge;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Integer getCodigoIbge() {
+        return codigoIbge;
+    }
+
+    public void setCodigoIbge(Integer codigoIbge) {
+        this.codigoIbge = codigoIbge;
+    }
+
+    @Override
+    public String toString() {
+        return getSigla(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/model/dao/EstadoDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import java.util.List;
+import model.bean.Estado;
+import static nfe.NFe.emBs;
+
+/**
+ *
+ * @author Joe
+ */
+public class EstadoDAO {
+    
+    public List<Estado> findAll(){
+        List<Estado> estados = null;
+        
+        try {
+            estados = emBs.createQuery("from Estado e").getResultList();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        
+        return estados;
+    }
+    
+}
